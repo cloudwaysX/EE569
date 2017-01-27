0e13d96af2a60542ef6e0a9ed914e7bb35b42428
cloudwaysX <yifang@usc.edu>
2017-01-26 23:41:34 -0800

    rgb to cmy(k) finished

--- hw1.sh
+++ hw1.sh
@@ -7,3 +7,8 @@
 --composition
 ./Problem1_a_composition 220 ./result/problem1/mirrored_dog.raw ./images/problem1/beach.raw ./result/problem1/dog_beach_composition.raw 300 808 1914
 --need further modification
+
+--rgbTOcmy(k)
+./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_C.raw 384 512 0
+./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_M.raw 384 512 1
+./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_Y.raw 384 512 2
