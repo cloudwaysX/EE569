7c2ed6def25371908f2e482a2fee2c8c8bbbf66e
cloudwaysX <yifang@usc.edu>
2017-01-28 11:14:21 -0800

    rgb to hsl finished with small error

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 g++ -o Problem1_a_mirror Problem1_a_mirror.cpp 
 
 g++ -o Problem1_a_composition Problem1_a_composition.cpp 
+
+g++ -o Problem1_b_rgb2cmk Problem1_b_rgb2cmk.cpp 
+
+g++ -o Problem1_b_rgb2hsl Problem1_b_rgb2hsl.cpp 
--- hw1.sh
+++ hw1.sh
@@ -12,3 +12,19 @@
 ./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_C.raw 384 512 0
 ./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_M.raw 384 512 1
 ./Problem1_b_rgb2cmk ./images/problem1/parrot.raw ./result/problem1/parrot_Y.raw 384 512 2
+
+./Problem1_b_rgb2cmk ./images/problem1/building.raw ./result/problem1/building_Y.raw 600 789 2
+./Problem1_b_rgb2cmk ./images/problem1/building.raw ./result/problem1/building_M.raw 600 789 1
+./Problem1_b_rgb2cmk ./images/problem1/building.raw ./result/problem1/building_C.raw 600 789 0
+
+--rgb to hsl !!!!!!!! Error with L
+
+./Problem1_b_rgb2hsl ./images/problem1/cat.raw ./result/problem1/cat_H.raw 398 600 0
+./Problem1_b_rgb2hsl ./images/problem1/cat.raw ./result/problem1/cat_S.raw 398 600 1
+./Problem1_b_rgb2hsl ./images/problem1/cat.raw ./result/problem1/cat_L.raw 398 600 2 ----????
+
+./Problem1_b_rgb2hsl ./images/problem1/dolphin.raw ./result/problem1/dolphin_H.raw 398 600 0
+./Problem1_b_rgb2hsl ./images/problem1/dolphin.raw ./result/problem1/dolphin_S.raw 398 600 1
+./Problem1_b_rgb2hsl ./images/problem1/dolphin.raw ./result/problem1/dolphin_L.raw 398 600 2 ----????
+
+
