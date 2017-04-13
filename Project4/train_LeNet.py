9ae1b895e1379f9f3de3fe34cdd7945c036cbdaa
cloudwaysX <yifang@usc.edu>
2017-04-13 11:26:46 -0700

    build LeNet5 finished

--- Project4/train_LeNet.py
+++ Project4/train_LeNet.py
@@ -1,16 +1,84 @@
-import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# #mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)  # Store mnist data for this tutorial
+def unpickle(filename):
+	import cPickle
+	import os
+	root="./cifar-10-batches-py"
+	filename = os.path.join(root,filename)
+	with open(filename,'rb') as f:
+		dict = cPickle.load(f)
+	return dict
 
-learning_rate = 0.01
-training_epochs = 1
-batch_size = 100
-display_step = 1
-logs_path = './logFiles'
+training_data=None
+training_labels=None
+
+#--------------------image and labels loading------------------------------------------#
+for i in xrange(1,6,1):
+	index="%d" %(i,)
+	filename = 'data_batch_'+index
+	trainingDict=unpickle(filename)
+	cur_training_data=trainingDict['data']
+	cur_training_labels=np.asarray(trainingDict['labels'])
+
+	cur_training_data=cur_training_data.reshape(cur_training_data.shape[0],3,32,32)
+
+	if training_data is None:
+		training_data = cur_training_data
+	else:
+		training_data=np.concatenate((training_data,cur_training_data),axis=0)
 
 
+	if training_labels is None:
+		training_labels = cur_training_labels
+	else:
+		training_labels=np.concatenate((training_labels,cur_training_labels),axis=0)
+
+training_data=training_data.transpose(0,2,3,1)
+# Finally we get training images as 40000x32x32x3 and training labels 40000
+
+filename = 'test_batch'
+testingDict=unpickle(filename)
+testing_data=testingDict['data']
+#print(testing_data.shape)
+testing_data=(testing_data.reshape(testing_data.shape[0],3,32,32)).transpose(0,2,3,1)
+testing_labels=np.asarray(testingDict['labels'])
+
+print('\n')
+print("Image Shape: {}".format(training_data[0].shape))
+print('\n')
+print("Training Set:   {} samples".format(len(training_data)))
+print("Testing Set:       {} samples".format(len(testing_data)))
+
+
+# -----------------Preprosessing-------------------------------------#
+
+from sklearn.utils import shuffle
+
+training_data, training_labels = shuffle(training_data, training_labels)
+
+# ---------------------Data Visualization----------------------------------------------------#
+
+
+# import random
+# import matplotlib.pyplot as plt
+# index = random.randint(0, len(testing_data))
+# image = testing_data[index].squeeze()
+
+# plt.figure(figsize=(1,1))
+# plt.imshow(image)
+# print(testing_labels[index])
+# plt.show()
+# #plt.savefig('random_sample.png')
+
+
+#----------------------Setup Tensorflow------------------------------------------------------#
+
+
+import tensorflow as tf
+
+training_epochs = 50
+BATCH_SIZE = 128
 
 def LeNet5(x):
 
@@ -31,7 +99,7 @@ def LeNet5(x):
 		#Layer2: Pooling. Input = 28x28x6. Output = 14x14x6.
 		pool1=tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-		#Layer3: Convolutional. Input = 14x14x5. Output = 10x10x16.
+		# Layer3: Convolutional. Input = 14x14x5. Output = 10x10x16.
 		# conv2_W = tf.get_variable("conv2_W", shape=(5,5,6,16),
   #          initializer=tf.contrib.layers.xavier_initializer())
   		conv2_W = tf.Variable(tf.truncated_normal(shape = [5,5,6,16], mean = mu, stddev = sigma))
@@ -68,186 +136,57 @@ def LeNet5(x):
     	pred=tf.matmul(fc2,softmax_W)+softmax_b
     	return pred
 
-def evaluation(testing_data,testing_labels,acc,x,y):
+# -------------------Train-------------------------#
 
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
 
-	num_examples = len(testing_labels)
-	avg_cost = 0
-	total_batch = int(num_examples/batch_size)
-	sess = tf.get_default_session()
+rate = 0.001
 
+logits = LeNet5(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
 
-	for i in range(total_batch):
-		batch_xs, batch_ys = testing_data[i*batch_size:(i+1)*batch_size],testing_labels[i*batch_size:(i+1)*batch_size]
-		c = sess.run(acc, feed_dict={x: batch_xs, y: batch_ys})
-		avg_cost += c / total_batch
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
 
-	return avg_cost
-
-
-def train_LeNet5(training_data,training_labels,testing_data,testing_labels):
-
-	x=tf.placeholder(tf.float32,[None,32,32,3],name = "InputData")
-	y=tf.placeholder(tf.int32,[None])
-	one_hot_y = tf.one_hot(y, 10)#?????
-
-
-
-	pred = LeNet5(x)
-
-	with tf.name_scope('Loss'):
-		# Minimize error using cross entropy
-		#cost=tf.reduce_mean(-tf.reduce_sum(one_hot_y*tf.log(pred), reduction_indices=1)) #??????????????////
-		cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, one_hot_y))
-		# print("aaa \n")
-		# print(-tf.reduce_sum(one_hot_y*tf.log(pred), reduction_indices=1))
-	with tf.name_scope('SGD'):
-    	# Gradient Descent
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-	with tf.name_scope('Accuracy'):
-    	# Accuracy
-		acc = tf.equal(tf.argmax(pred, 1), tf.argmax(one_hot_y, 1))
-		acc = tf.reduce_mean(tf.cast(acc, tf.float32))
-
-	# with tf.name_scope('mAP'):
-	# 	from tensorflow.metrics import sparse_average_precision_at_k as mAP_k
-	# 	true_y=tf.argmax(one_hot_y, 1)
-	# 	pred_y=tf.argmax(pred, 1)
-	# 	mAP=mAP_k(true_y.reshape(1,len(true_y)),pred_y.reshape(1,len(true_y)),k=len(true_y))
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
 
 
 
-	# Initializing the variables
-	init = tf.global_variables_initializer()
-
-	# Create a summary to monitor cost tensor
-	tf.summary.scalar("loss", cost)
-	# Create a summary to monitor accuracy tensor
-	tf.summary.scalar("accuracy", acc)
-	# Merge all summaries into a single op
-	merged_summary_op = tf.summary.merge_all()
-
-	# Launch the graph
-	with tf.Session() as sess:
-		sess.run(init)
-
-    		# op to write logs to Tensorboard
-		summary_writer = tf.summary.FileWriter(logs_path,graph = tf.get_default_graph())
-
-    		# Training cycle
-		for epoch in range(training_epochs):
-			avg_cost = 0.
-			total_batch = int(len(training_data)/batch_size)
-			# Loop over all batches
-			for i in range(total_batch):
-				batch_xs, batch_ys = training_data[i*batch_size:(i+1)*batch_size],training_labels[i*batch_size:(i+1)*batch_size]
-				# Run optimization op (backprop), cost op (to get loss value)
-				# and summary nodes
-				_, c, summary = sess.run([optimizer, cost, merged_summary_op],feed_dict={x: batch_xs, y: batch_ys})#????
-				# Write logs at every iteration
-				summary_writer.add_summary(summary, epoch * total_batch + i)
-				# Compute average loss
-
-				avg_cost += c / total_batch
-			
-			# Display logs per epoch step
-			if (epoch+1) % display_step == 0:
-				print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-
-		print("Optimization Finished!")
-
-		# Test model
-		# Accuracy of 0.91 -> 91%
-		print("Accuracy:", evaluation(testing_data,testing_labels,acc,x,y))
-
-		# testing_mAP,_=sess.run(mAP)
-
-		# print("mAP: ",testing_mAP)
-
-
-def unpickle(filename):
-	import cPickle
-	import os
-	root="./cifar-10-batches-py"
-	filename = os.path.join(root,filename)
-	with open(filename,'rb') as f:
-		dict = cPickle.load(f)
-	return dict
-
-
-#----------------------------------------------------------------------------------------------------#
-# Main function:
-def main():
-	#--------------------image and labels loading------------------------------------------#
-	for i in xrange(1,5,1):
-		index="%d" %(i,)
-		filename = 'data_batch_'+index
-		trainingDict=unpickle(filename)
-		cur_training_data=trainingDict['data']
-		cur_training_labels=np.asarray(trainingDict['labels'])
-
-		cur_training_data=cur_training_data.reshape(cur_training_data.shape[0],3,32,32)
-
-		try:
-			training_data
-		except NameError:
-			training_data = cur_training_data
-		else:
-			training_data=np.concatenate((training_data,cur_training_data),axis=0)
-
-
-		try:
-			training_labels
-		except NameError:
-			training_labels = cur_training_labels
-		else:
-			training_labels=np.concatenate((training_labels,cur_training_labels),axis=0)
-
-	training_data=training_data.transpose(0,2,3,1)
-	# Finally we get training images as 40000x32x32x3 and training labels 40000
-
-	filename = 'test_batch'
-	testingDict=unpickle(filename)
-	testing_data=testingDict['data']
-	#print(testing_data.shape)
-	testing_data=(testing_data.reshape(testing_data.shape[0],3,32,32)).transpose(0,2,3,1)
-	testing_labels=np.asarray(testingDict['labels'])
-
-	print('\n')
-	print("Image Shape: {}".format(training_data[0].shape))
-	print('\n')
-	print("Training Set:   {} samples".format(len(training_data)))
-	print("Testing Set:       {} samples".format(len(testing_data)))
-
-	#---------------------Data Visualization----------------------------------------------------#
-
-
-	# import random
-	# import matplotlib.pyplot as plt
-	# index = random.randint(0, len(training_data))
-	# image = training_data[index].squeeze()
-
-	# plt.figure(figsize=(1,1))
-	# plt.imshow(image)
-	# plt.show()
-	# #plt.savefig('random_sample.png')
-	# print(training_labels[index])
-
-
-	#-----------------Preprosessing-------------------------------------#
-
-	from sklearn.utils import shuffle
-
-	training_data, training_labels = shuffle(training_data, training_labels)
-
-	#---------------Training and evaluation----------------------------------------#
-	train_LeNet5(training_data,training_labels,testing_data,testing_labels)
-
-
-#----------------------------------------------------------------------------------------------------#
-# Boilerplate syntax:
-if __name__ == '__main__':
-	main()
-
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(training_data)
+    
+    print("Training...")
+    print()
+    for i in range(training_epochs):
+        training_data,training_labels = shuffle(training_data,training_labels)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = training_data[offset:end], training_labels[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            
+        train_accuracy = evaluate(training_data,training_labels)
+    	test_accuracy = evaluate(testing_data, testing_labels)
+    	print("EPOCH {} ...".format(i+1))
+    	print("test Accuracy = {:.3f}".format(test_accuracy))
+    	print("train Accuracy = {:.3f}".format(train_accuracy))
+    	print()
+        
+    saver.save(sess, 'lenet')
+    print("Model saved")
 
 
